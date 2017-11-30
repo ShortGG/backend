ceb0cb45e669d8e0fc997382f971721d5f3d17ff
Aksels Ledins <aksels.ledins@gmail.com>
2017-11-30 23:16:39 +0100

    (server.go) add configurable cors

--- server.go
+++ server.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	handlers "./handlers"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{os.Getenv("CORS_URL")},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE, echo.OPTIONS},
+	}))
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "short.gg API")
